c1c3710aa84cbe0f843fb08578fc50c86265127f
nguyenson2012 <traibachkhoa2012@gmail.com>
2016-01-31 20:26:34 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyenthanhson/youtubeapidemo/Config.java
@@ -0,0 +1,13 @@
+package com.nguyenthanhson.youtubeapidemo;
+
+/**
+ * Created by SON on 12/19/2015.
+ */
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyCbgTU7Ks7NIFaumrAJZaRPDhp_Vg5RqKM";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "4MSoe_k8F6Q";
+}
